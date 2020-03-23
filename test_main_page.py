f1fa9faf790ad270e9934f444b367b40e9ab6991
AlexOkrepkyi <creating.smth@gmail.com>
2020-03-23 15:47:23 +0200

    first POM shot

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,15 @@
+from pages.main_page import MainPage
+
+
 def test_guest_can_go_to_login_page(driver):
-
     link = "http://selenium1py.pythonanywhere.com/"
-    driver.get(link)
-    login_link = driver.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(driver, link)
+    page.open()
+    page.go_to_login_page()
 
+
+def test_guest_should_see_login_link(driver):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(driver, link)
+    page.open()
+    page.should_be_login_link()
