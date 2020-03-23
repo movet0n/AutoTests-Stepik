fc8516166a7be48ebc491c6bcbb228da49299b11
AlexOkrepkyi <creating.smth@gmail.com>
2020-03-23 11:17:22 +0200

    deleted redundant files

--- test_items.py
+++
@@ -1,7 +0,0 @@
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-
-
-def test_test_button_is_present(driver):
-
-    driver.get(link)
-    driver.find_element_by_css_selector(".add-to-basket .btn").is_displayed()
