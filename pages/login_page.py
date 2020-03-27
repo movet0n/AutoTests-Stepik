78ac0263433f371e3ae1a59a16a9d0f114d8a1b6
AlexOkrepkyi <creating.smth@gmail.com>
2020-03-27 12:34:10 +0200

    switched positions of the imported libraries

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,8 +1,16 @@
-from pages.base_page import BasePage
 from pages.locators import LoginPageLocators
+from pages.base_page import BasePage
 
 
 class LoginPage(BasePage):
+
+    def register_new_user(self, email, password):
+        self.email = email
+        self.password = password
+        self.driver.find_element(*LoginPageLocators.REG_EMAIL_FIELD).send_keys(email)
+        self.driver.find_element(*LoginPageLocators.REG_PASSWORD_1_FIELD).send_keys(password)
+        self.driver.find_element(*LoginPageLocators.REG_PASSWORD_2_FIELD).send_keys(password)
+        self.driver.find_element(*LoginPageLocators.REGISTER_BUTTON).click()
 
     def should_be_login_page(self):
         self.should_be_login_url()
@@ -13,10 +21,10 @@ class LoginPage(BasePage):
         assert "/login" in self.driver.current_url
 
     def should_be_login_form(self):
-        assert self.is_element_present(*LoginPageLocators.LOGIN_EMAIL), "'login email' field is not presented"
-        assert self.is_element_present(*LoginPageLocators.LOGIN_PASSWORD), "'login password' field is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_EMAIL_FIELD), "'login email' field is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_PASSWORD_FIELD), "'login password' field is not presented"
 
     def should_be_register_form(self):
-        assert self.is_element_present(*LoginPageLocators.REG_EMAIL), "'reg email' field is not presented"
-        assert self.is_element_present(*LoginPageLocators.REG_PASSWORD_1), "'reg password' field is not presented"
-        assert self.is_element_present(*LoginPageLocators.REG_PASSWORD_2), "'reg confirm password' field is not presented"
+        assert self.is_element_present(*LoginPageLocators.REG_EMAIL_FIELD), "'reg email' field is not presented"
+        assert self.is_element_present(*LoginPageLocators.REG_PASSWORD_1_FIELD), "'reg password' field is not presented"
+        assert self.is_element_present(*LoginPageLocators.REG_PASSWORD_2_FIELD), "'reg confirm password' field is not presented"
