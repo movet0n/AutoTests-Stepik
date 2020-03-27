12e919f0346d0fb2adcd74e554e581af20a0cd9c
AlexOkrepkyi <creating.smth@gmail.com>
2020-03-27 12:35:30 +0200

    positioned all the tests as stated in the lesson requirements

--- test_product_page.py
+++ test_product_page.py
@@ -31,20 +31,20 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.verify_success_message_is_not_present()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, driver):
         link = ProductPageLocators.PRODUCT_LINK
         page = ProductPage(driver, link)
         page.open()
         page.is_present_add_to_basket_button()
         page.add_to_basket()
-        page.solve_quiz_and_get_code()
         page.verify_book_titles_are_similar()
         page.verify_success_message_is_present()
         page.verify_deferred_benefit_offer_is_present()
         page.verify_book_prices_are_similar()
 
 
-@pytest.mark.skip
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -68,15 +68,7 @@ def test_guest_can_add_product_to_basket(driver, link):
     page.verify_book_prices_are_similar()
 
 
-@pytest.mark.skip
-def test_guest_should_see_login_link_on_product_page(driver):
-    link = ProductPageLocators.THE_CITY_AND_THE_STARS_LINK
-    page = ProductPage(driver, link)
-    page.open()
-    page.should_be_login_link()
-
-
-@pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(driver):
     link = ProductPageLocators.THE_CITY_AND_THE_STARS_LINK
     page = ProductPage(driver, link)
@@ -84,7 +76,7 @@ def test_guest_can_go_to_login_page_from_product_page(driver):
     page.go_to_login_page()
 
 
-@pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(driver):
     link = ProductPageLocators.PRODUCT_LINK
     page = ProductPage(driver, link)
@@ -93,3 +85,36 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(driver):
 
     basket_page = BasketPage(driver, url=driver.current_url)
     basket_page.verify_basket_is_empty()
+
+
+@pytest.mark.need_review
+def test_guest_should_see_login_link_on_product_page(driver):
+    link = ProductPageLocators.THE_CITY_AND_THE_STARS_LINK
+    page = ProductPage(driver, link)
+    page.open()
+    page.should_be_login_link()
+
+
+@pytest.mark.xfail(reason="success message should be present after adding a book to the basket")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(driver):
+    link = ProductPageLocators.PRODUCT_LINK
+    page = ProductPage(driver, link)
+    page.open()
+    page.add_to_basket()
+    page.verify_success_message_is_not_present()
+
+
+def test_guest_cant_see_success_message(driver):
+    link = ProductPageLocators.PRODUCT_LINK
+    page = ProductPage(driver, link)
+    page.open()
+    page.verify_success_message_is_not_present()
+
+
+@pytest.mark.xfail(reason="success message should not disappear")
+def test_message_disappeared_after_adding_product_to_basket(driver):
+    link = ProductPageLocators.PRODUCT_LINK
+    page = ProductPage(driver, link)
+    page.open()
+    page.add_to_basket()
+    page.verify_success_message_is_not_present_with_disappear_function()
